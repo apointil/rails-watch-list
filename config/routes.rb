60c530382c6769ae3f7a798bde759ae9a01fef27
Adrien Pointillart <apointillart@gmail.com>
2022-02-17 16:59:33 +0100

    green rake

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, except: [:index] do
-    resources :bookmarks
+  resources :lists do
+    resources :bookmarks, only: %w(new create)
   end
+  resources :bookmarks, only: :destroy
 end
